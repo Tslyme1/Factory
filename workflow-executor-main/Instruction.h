a900e73658b2a6f9d80ee4eb88f247fea4973166
Tslyme1 <96186227+Tslyme1@users.noreply.github.com>
2021-12-18 01:49:18 +0700

    instruction registration

--- workflow-executor-main/Instruction.h
+++ workflow-executor-main/Instruction.h
@@ -1,33 +1,63 @@
 #pragma once
+#include "InstructionFactory.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
 
+typedef struct Protocol { bool takes; bool returns; } Protocol;
+
 class Instruction {
 public:
 	virtual void execute(std::string& text, std::vector<std::string> args) = 0;
+	virtual const Protocol getProtocol() = 0;
 };
 
 class Readfile : public Instruction {
 	void execute(std::string& text, std::vector<std::string> args) override;
-};
-
-class Writefile : public Instruction {
-	void execute(std::string& text, std::vector<std::string> args) override;
+	inline const Protocol getProtocol() {return Protocol{ false, true };}
 };
 
 class Grep : public Instruction {
 	void execute(std::string& text, std::vector<std::string> args) override;
+	inline const Protocol getProtocol() { return Protocol{ true, true }; };
 };
 
 class Sort : public Instruction {
 	void execute(std::string& text, std::vector<std::string> args) override;
+	inline const Protocol getProtocol() { return Protocol{ true, true }; };
 };
 
 class Replace : public Instruction {
 	void execute(std::string& text, std::vector<std::string> args) override;
+	inline const Protocol getProtocol() { return Protocol{ true, true }; };
 };
 
-class Dump : public Instruction {
+class DataWriter : public Instruction {
 	void execute(std::string& text, std::vector<std::string> args) override;
 };
+
+class Writefile : public DataWriter {
+	inline const Protocol getProtocol() { return Protocol{ true, false }; };
+};
+
+class Dump : public DataWriter {
+	inline const Protocol getProtocol() { return Protocol{ true, true }; };
+};
+
+namespace {
+	bool registerInstructions() {
+		InstructionFactory& Factory = InstructionFactory::getInstance();
+
+		Factory.Register("readfile",  new Readfile());
+		Factory.Register("writefile", new Writefile());
+		Factory.Register("grep",      new Grep());
+		Factory.Register("sort",      new Sort());
+		Factory.Register("replace",   new Replace());
+		Factory.Register("dump",      new Dump());
+
+		return true;
+	}
+
+	static bool fakeVar = registerInstructions();
+}
