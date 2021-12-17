499e383902655295c72a1b202f7d2ba77b9ec93f
Tslyme1 <96186227+Tslyme1@users.noreply.github.com>
2021-12-17 19:47:33 +0700

    no using in header

--- workflow-executor-main/Workflow.h
+++ workflow-executor-main/Workflow.h
@@ -5,11 +5,8 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 typedef struct Workflow {
-	unordered_map<size_t, vector<string> > instruction_args;
-	unordered_map<size_t, Instruction*> instruction_bind;
-	vector<size_t> order;
+	std::unordered_map<size_t, std::vector<std::string> > instruction_args;
+	std::unordered_map<size_t, Instruction*> instruction_bind;
+	std::vector<size_t> order;
 } Workflow;
-
