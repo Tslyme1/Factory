97d60d2ec13058879024283c41c1226cb6c738d4
Tslyme1 <96186227+Tslyme1@users.noreply.github.com>
2021-12-18 01:52:20 +0700

    builds intructions + flow validation

--- workflow-executor-main/WorkflowExecutor.cpp
+++ workflow-executor-main/WorkflowExecutor.cpp
@@ -1,15 +1,26 @@
 #include "WorkflowExecutor.h"
 #include "Workflow.h"
+#include "InstructionFactory.h"
 
+#include <stdexcept>
 #include <iostream>
-#include <tuple>
-#include <utility>
-#include <functional>
 
 void WorkflowExecutor::run(Workflow w) const {
+	bool data_flows = false;
 	std::string text = "";
 	
+	InstructionFactory& Factory = InstructionFactory::getInstance();
 	for (size_t id : w.order) {
-		w.instruction_bind[id]->execute(text, w.instruction_args[id]);
+		Instruction* istruct = Factory.Build(w.instruction_bind[id]);
+		const auto p = istruct->getProtocol();
+		
+		if (data_flows != p.takes) {
+			throw std::invalid_argument("wrong instruction order at bind `" 
+			                            + std::to_string(id) + " = " + w.instruction_bind[id] + "`");
+		}
+
+		istruct->execute(text, w.instruction_args[id]);
+
+		data_flows = p.returns;
 	}
 };
